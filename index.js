0b8384688b19fef7bfee7a3f15c8704026c24e5d
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-08-25 15:40:44 +0800

    try

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // Parse JSON requests
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.post('/api/webhook', (req, res) => {
+  const payload = req.body; // Received webhook data
+  console.log('Webhook Payload:', payload); // Log the payload
+  res.send('Webhook received');
+});
 
 app.listen(port, () => {
-  console.log(`Server is running at ${port}`)
-})
+  console.log(`Server is running at ${port}`);
+});

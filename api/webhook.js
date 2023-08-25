3ba84fffcb638bdb91fdf486dfe0293bf88863ec
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-08-25 16:07:31 +0800

    try

--- api/webhook.js
+++ api/webhook.js
@@ -5,12 +5,13 @@ const port = 3000;
 app.use(express.json()); // Parse JSON requests
 
 app.post('/api/webhook', (req, res) => {
-  const payload = req.body; // Received webhook data
-  if (payload && payload.topic === 'products/create') {
-    const productData = payload.data; // Product data
-    console.log('New product created:', productData);
-  }
-  res.send('Webhook received');
+  // const payload = req.body; // Received webhook data
+  console.log("try");
+  // if (payload && payload.topic === 'products/create') {
+  //   const productData = payload.data; // Product data
+  //   console.log('New product created:', productData);
+  // }
+  // res.send('Webhook received');
 });
 
 app.listen(port, () => {
